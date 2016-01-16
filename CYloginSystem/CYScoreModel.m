91c2dfe895e5cfc5a9bacc1c0bac41288973528c
yong412416 <18363010295@163.com>
2016-01-16 17:11:47 +0800

    116-17-11

---
+++ CYloginSystem/CYScoreModel.m
@@ -0,0 +1,13 @@
+//
+//  CYScoreModel.m
+//  CYloginSystem
+//
+//  Created by qianfeng on 16/1/12.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import "CYScoreModel.h"
+
+@implementation CYScoreModel
+
+@end
